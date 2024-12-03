1bfcc7fd0eab08cf82ffc33f86782f16289a0f58
Sivarajan <citysiva180@users.noreply.github.com>
2024-12-03 07:15:17 +0530

    ds_and_algo : code tested!

--- linked_list_fullcode.py
+++ linked_list_fullcode.py
@@ -30,7 +30,7 @@ class LinkedList:
         self.length += 1
         return True
 
-    def prepend_items(self, value):
+    def prepend(self, value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -94,7 +94,7 @@ class LinkedList:
             return self.append_items(value)
         new_node = Node(value)
         temp = self.get(index-1)
-        new_node.next = temp
+        new_node.next = temp.next
         temp.next = new_node
         self.length += 1
         return True
@@ -107,7 +107,10 @@ class LinkedList:
         if index == self.length:
             return self.pop_items()
         prev = self.get(index-1)
-        temp, prev.next = prev.next, temp.next
+        # temp, prev.next = prev.next, temp.next
+        temp = prev.next
+        prev.next = temp.next
+
         temp.next = None
         self.length -= 1
         return True
@@ -123,3 +126,77 @@ class LinkedList:
             temp.next = before
             before = temp
             temp = after
+
+
+linked_list = LinkedList(4)
+
+
+# Adding elements
+linked_list.append_items(1)
+linked_list.append_items(2)
+
+print(linked_list.pop_items())
+print(linked_list.pop_items())
+print(linked_list.pop_items())
+
+
+linked_list.print_list()
+
+
+linked_list.append_items(2)
+linked_list.append_items(3)
+linked_list.append_items(4)
+linked_list.append_items(5)
+linked_list.append_items(6)
+linked_list.append_items(7)
+linked_list.append_items(8)
+linked_list.append_items(9)
+
+print("after adding elements :  ")
+
+# printing elements
+
+linked_list.print_list()
+
+
+# pop items in linked list
+
+linked_list.pop_items()
+linked_list.pop_items()
+linked_list.pop_items()
+linked_list.pop_items()
+linked_list.pop_items()
+
+print("After removing items ")
+linked_list.print_list()
+
+
+# prepending items in linked list
+
+linked_list.prepend(12)
+linked_list.prepend(11)
+linked_list.prepend(10)
+
+print("After prepending items ")
+linked_list.print_list()
+
+
+print("getting elements")
+getting_elements = linked_list.get(2)
+print(getting_elements.value)  # type: ignore
+getting_elements = linked_list.get(-1)
+print(getting_elements)
+
+print("inserting items")
+linked_list.insert(2, 456)
+linked_list.print_list()
+
+print("after removing")
+
+linked_list.remove(2)
+linked_list.print_list()
+
+
+print("after reversing")
+linked_list.reverse()
+linked_list.print_list()
